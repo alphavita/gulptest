9820056a5b1482f36803e3796d95f42c58633347
alphavita <abcdef.vita2012@yandex.ru>
2016-09-28 21:07:41 +0300

    Add files via upload

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,2 +1,32 @@
-;console.log('o');
+;
+console.log('map'); // Как только будет загружен API и готов DOM, выполняем инициализацию
+ymaps.ready(init);
 
+// Инициализация и уничтожение карты при нажатии на кнопку.
+function init () {
+    var myMap;
+     var myPlacemark;
+
+    $('#toggle').bind({
+        click: function () {
+            if (!myMap) {
+                myMap = new ymaps.Map('map', {
+                    center: [50.42562807, 30.53356600], // Киев
+                    zoom: 16
+                }, {
+                    searchControlProvider: 'yandex#search'
+                });
+                // $("#toggle").attr('value', 'Скрыть карту');
+
+            }
+            else {
+                myMap.destroy();// Деструктор карты
+                myMap = null;
+
+                // $("#toggle").attr('value', 'Показать карту снова');
+            }
+        }
+    });
+
+
+}
